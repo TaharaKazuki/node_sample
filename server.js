ac2b8d3861a7ecce64f77dd421890423a80ae2c0
田原 一樹 <ssp0727.lnc@gmail.com>
2017-12-19 09:28:23 +0900

    簡易掲示板作成

--- server.js
+++ server.js
@@ -1,20 +1,40 @@
+// nodeのコアモジュールのhttpを使う
 var http = require('http');
+var ejs  = require('ejs');
+var qs   = require('querystring');
 var fs     = require('fs');
 var config = require('./config');
 var server = http.createServer();
-var msg;
+var posts = [];
 
-server.on('request', function(req, res) {
-  fs.readFile(__dirname + '/hello.html', 'utf-8', function (err, data) {
-    if (err) {
-        res.writeHead(404, {'Content-Type' : 'text/plain'});
-        res.write('page not found');
-        return res.end();
-    }
-    res.writeHead(200, {'Content-Type' : 'text/html'});
-    res.write(data);
-    res.end();
+var template = fs.readFileSync(__dirname + '/hello.ejs', 'utf-8');
+
+function renderForm(posts, res) {
+  var data = ejs.render(template, {
+      posts: posts
   });
-});
 
+  res.writeHead(200, {'Content-Type' : 'text/html'});
+  res.write(data);
+  res.end();
+};
+
+server.on('request', (req, res)=> {
+  if (req.method === 'POST') {
+    req.data = "";
+    req.on("readable", ()=> {
+      req.data += req.read() || '';
+      console.log(req.data);
+    });
+    req.on("end", ()=> {
+      var query = qs.parse(req.data);
+      console.log(query);
+      posts.push(query.user_name);
+      renderForm(posts, res);
+    });
+  } else {
+    renderForm(posts, res);
+  }
+});
 server.listen(config.port);
+console.log('------ server start -------');
